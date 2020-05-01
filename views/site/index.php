00af3b1863ca857f544024ab359f9d3d0c3b3313
dd08997 <dd08997@gmail.com>
2020-05-01 07:58:42 +0300

    init yii minimal

---
+++ views/site/index.php
@@ -0,0 +1,3 @@
+
+
+<h2>Cheburnet starter</h2>
--- web/index.php
+++ web/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo '<h1>Video School</h1>';
+define ('YII_DEBUG', true); // режим debug для вывода на экран подробных сообщений об ошибках
+require (__DIR__ . '/../vendor/autoload.php'); // проверяет классы, если конкретный класс не загружен то загружает
+require (__DIR__ . '/../vendor/yiisoft/yii2/Yii.php'); // шдавный модуль yii фреймворка
+$config = require (__DIR__ . '/../config/web.php'); // загрузка конфигуратора
+$app = new yii\web\Application($config); // запуск приложения с конфигурацией
+$app-> run(); // запуск
